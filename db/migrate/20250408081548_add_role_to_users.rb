6c4f27ee1e50404616e9fe228aaa35663a9398f7
koheihotta191 <kobukuroayaka@gmail.com>
2025-04-10 21:40:59 +0900

    デプロイ修正2

--- db/migrate/20250408081548_add_role_to_users.rb
+++ db/migrate/20250408081548_add_role_to_users.rb
@@ -1,5 +1,6 @@
 class AddRoleToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :role, :integer, default: 0, null: false
+    # add_column :users, :role, :integer, default: 0, null: false
+    change_column_default :users, :role, from: nil, to: 0
   end
 end
