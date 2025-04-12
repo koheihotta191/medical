debb94f72566644e59bace6174723dfc7ff8551b
koheihotta191 <kobukuroayaka@gmail.com>
2025-04-13 01:10:08 +0900

    デプロイ修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable
 
   validates :staff_id, presence: true
   validates :staff_id, uniqueness: true
