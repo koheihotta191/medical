f6ed8a878332a69b7ed2041d28654ed7be4d9b9b
koheihotta191 <kobukuroayaka@gmail.com>
2025-05-19 01:21:20 +0900

    ユーザー新規登録画面

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,21 +1,21 @@
 class UserPolicy < ApplicationPolicy
   def index?
-    user.admin? # 管理者のみ許可
+    user.role == 0 # 管理者のみ許可
   end
 
   def create?
-    user.admin? # 管理者のみ許可
+    user.role == 0 # 管理者のみ許可
   end
 
   def new?
-    create? # create?と同じ権限
+    user.role == 0 # create?と同じ権限
   end
 
   def update?
-    user.admin? # 管理者のみ許可
+    user.role == 0 # 管理者のみ許可
   end
 
   def destroy?
-    user.admin? # 管理者のみ許可
+    user.role == 0 # 管理者のみ許可
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  
 
   resources :patients do
     resources :medical_records
     resources :nursing_records
     resources :care_records
+    resources :users, only: [:new, :create]
   end
 
   root "patients#index"
